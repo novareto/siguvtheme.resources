8810efeb201727c8a37b3f72790226dc2b9cf292
goschtl <ck@novareto.de>
2015-05-11 10:37:53 +0200

    changes css

--- src/siguvtheme/resources/__init__.py
+++ src/siguvtheme/resources/__init__.py
@@ -2,19 +2,18 @@
 
 import logging
 from fanstatic import Library, Resource, Group
-from js.jquery import jquery
-from uvc.siguvtheme.resources import css, js, pretty, inset
+#from js.jquery import jquery
+#from uvc.siguvtheme.resources import css, js, pretty, inset
 
 
 library = Library('siguvtheme.resources', 'static')
-maincss = Resource(library, 'main.css', depends=[css])
-mainjs = Resource(library, 'main.js', depends=[js, jquery])
-tune = Group([maincss, pretty, inset, mainjs])
+maincss = Resource(library, 'main.css')
+mainjs = Resource(library, 'main.js')
+siguvcss = Resource(library, 'siguvtheme.css')
+tune = Group([maincss, siguvcss, mainjs])
 
 logger = logging.getLogger('uvcsite.bg.siguvtheme')
 
 
 def log(message, summary='', severity=logging.DEBUG):
     logger.log(severity, '%s %s', summary, message)
-
-
